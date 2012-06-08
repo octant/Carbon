ab1a970fe9e2130fcbf13ecb6cc8a136d44bb3b6
Michael Wood <octant.coder@gmail.com>
2012-06-08 15:35:46 -0400

    Changed scope for personalities to last_seen

--- app/models/personality.rb
+++ app/models/personality.rb
@@ -7,7 +7,7 @@ class Personality < ActiveRecord::Base
   default_scope :order => 'name ASC'
   scope :orphans, where(:device_id => nil)
   scope :nameless, where("ip LIKE ? AND name LIKE ?", "192.168.%.___", "%-%-%-%")
-  scope :missing_1_week, where("updated_at < ?", 1.week.ago)
-  scope :missing_1_month, where("updated_at < ?", 1.month.ago)
+  scope :missing_1_week, where("last_seen < ?", 1.week.ago)
+  scope :missing_1_month, where("last_seen < ?", 1.month.ago)
   scope :servers, joins(:device).where("devices.kind = ? or devices.kind = ?", "Server", "Virtual Datacenter")
 end
